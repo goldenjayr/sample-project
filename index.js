3eea54f283bd216a2f280d3215fcc1ff592d8237
Jay-R Joseph Gabunada <jayr@23point5.com>
2024-11-19 09:29:13 +0800

    added new scripts

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+// Add event listener to navigation links
+document.querySelectorAll('header nav a').forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const sectionId = link.getAttribute('href').slice(1);
+    document.querySelector(`section#${sectionId}`).scrollIntoView({ behavior: 'smooth' });
+  });
+});
